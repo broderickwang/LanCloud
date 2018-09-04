1d1be9ec4783396133c915294b4fd9d02a741aa8
Marc <wangchengda1990@gmail.com>
2018-09-04 22:48:10 +0800

    JWT 认证

---
+++ author/src/main/java/com/lancoder/auth/author/controller/LcAuthor.java
@@ -0,0 +1,13 @@
+package com.lancoder.auth.author.controller;
+
+/**
+ * @project: LanCloud
+ * @author: wangchengda
+ * @email: wangchengda1990@gmail.com
+ * @date: 2018/9/4
+ * @Description:
+ * @Copyright: 2018 broderickwang.github.io Inc. All rights reserved.
+ * @version: V1.0
+ */
+public class LcAuthor {
+}
---
+++ author/src/main/java/com/lancoder/auth/author/controller/LcAuthorController.java
@@ -0,0 +1,19 @@
+package com.lancoder.auth.author.controller;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+/**
+ * @project: LanCloud
+ * @author: wangchengda
+ * @email: wangchengda1990@gmail.com
+ * @date: 2018/9/4
+ * @Description:
+ * @Copyright: 2018 broderickwang.github.io Inc. All rights reserved.
+ * @version: V1.0
+ */
+public class LcAuthorController {
+
+}
